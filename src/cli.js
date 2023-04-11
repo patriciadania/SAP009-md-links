2920f76ed539936f0c10a1a29c7b70392fead1b3
patriciadania <patricia.adania@hotmail.com>
2023-04-11 18:01:16 -0300

    Atualização

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+// import chalk from "chalk";
+// import pegaArquivo from "./index.js";
+
+// const caminho = process.argv;
+
+// function processaTexto(caminho) {
+//     const resultado = pegaArquivo(caminho[2]);
+//     console.log(chalk.yellow('lista de links'), resultado);
+// }
+// processaTexto(caminho);
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import {fs} from 'fs';
+import chalk from 'chalk';
+
+// const fs = required('fs');
+// const chalk = required('chalk')
+
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+function extraiLinks(texto) {
+    const regex = /\\[[^[\]]*?\]\(https?:\/\/[^\s?#.].[^\s]*\)/gm;
+    const capturas = [...texto.matchAll(regex)]; //array expandido
+    const resultados = capturas.map(captura => ({ [captura[1]]: captura[2] }));
+    return resultados;
+}
+
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        return extraiLinks(texto);
+    } catch (erro) {
+        trataErro(erro)
+    }
+}
+
+export default pegaArquivo;
