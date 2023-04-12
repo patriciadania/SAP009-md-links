3636edeef50a5b34103d962bcd2cc246cd7a9a2d
patriciadania <patricia.adania@hotmail.com>
2023-04-12 19:49:43 -0300

    Validação de Links e Tratamento de funções

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,23 +1,15 @@
-import chalk from "chalk";
-
 function extraiLinks(arrLinks) {
     return arrLinks.map((objetoLink) => Object.values(objetoLink).join())
 }
 
-async function checaStatus(listaURLs) {
-    const arrStatus = await Promise
-        .all(
-            listaURLs.map(async (url) => {
-                try {
-                    const response = await fetch(url)
-                    return response.status;
-
-                } catch (erro) {
-                    return manejaErros(erro);
-                }
-            })
-        )
-    return arrStatus;
+function checaStatus(listaURLs) {
+    return Promise.all(
+        listaURLs.map((url) => {
+            return fetch(url)
+                .then(response => response.status)
+                .catch(erro => manejaErros(erro));
+        })
+    );
 }
 
 function manejaErros(erro) {
@@ -26,15 +18,14 @@ function manejaErros(erro) {
     } else {
         return ' Ocorreu um erro';
     }
-} 
-
-export default async function listaValidada(listaDeLinks) {
-    const links = extraiLinks(listaDeLinks);
-    const status = await checaStatus(links);
-
-    return listaDeLinks.map((objeto, indice) => ({
-        ...objeto,
-        status: status[indice]
-    }))
 }
 
+export default function listaValidada(listaDeLinks) {
+    const links = extraiLinks(listaDeLinks);
+    return checaStatus(links).then(status => {
+        return listaDeLinks.map((objeto, indice) => ({
+            ...objeto,
+            status: status[indice]
+        }));
+    });
+}
