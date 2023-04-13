6e94c748d5590ca884250b983c7f4452eb0d5e44
patriciadania <patricia.adania@hotmail.com>
2023-04-13 10:10:41 -0300

    Ajustes em funções e erros

--- arquivos/texto.md
+++ arquivos/texto.md
@@ -5,3 +5,6 @@ São geralmente recuperados a partir de um objeto [FileList](https://developer.m
 [Teste de retorno 400](https://httpstat.us/404).
 [gatinho salsicha](http://gatinhosalsicha.com.br/)
 
+vsgvyusgbdfviklk
+
+vjkzbdvuysd
--- src/cli.js
+++ src/cli.js
@@ -1,51 +1,61 @@
 import chalk from 'chalk';
 import fs from 'fs';
-import pegaArquivo from './index.js';
+import mdLinks from './index.js';
 import listaValidada from './http-validacao.js';
 
 const caminho = process.argv;
 
 function imprimeLista(valida, resultado, identificador = '') {
   if (valida) {
-    listaValidada(resultado).then(lista => {
-      console.log(
-        chalk.yellow('lista validada'),
-        chalk.black.bgGreen(identificador),
-        lista);
-    }).catch(console.error);
+    listaValidada(resultado)
+      .then(lista => {
+        console.log(
+          chalk.yellow('Lista validada'),
+          chalk.black.bgGreen(identificador),
+          lista);
+      })
+      .catch(console.error);
   } else {
     console.log(
-      chalk.yellow('lista de links'),
+      chalk.yellow('Lista de links'),
       chalk.black.bgGreen(identificador),
       resultado);
   }
 }
 
+function processaArquivo(nomeDoArquivo, valida) {
+  return mdLinks(nomeDoArquivo)
+    .then(resultado => imprimeLista(valida, resultado))
+    .catch(console.error);
+}
+
+function processaDiretorio(caminho, valida) {
+  return fs.promises.readdir(caminho)
+    .then(arquivos => {
+      arquivos.forEach(nomeDeArquivo => {
+        processaArquivo(`${caminho}/${nomeDeArquivo}`, valida);
+      });
+    })
+    .catch(console.error);
+}
+
 function processaTexto(argumentos) {
   const caminho = argumentos[2];
-  const valida = argumentos[3] === '--valida';
+  const valida = argumentos[3] === '--validate';
 
   try {
     fs.lstatSync(caminho);
   } catch (erro) {
     if (erro.code === 'ENOENT') {
-      console.log('arquivo ou diretório não existe');
+      console.log('Arquivo ou diretório não existe');
       return;
     }
   }
 
   if (fs.lstatSync(caminho).isFile()) {
-    pegaArquivo(argumentos[2]).then(resultado => {
-      imprimeLista(valida, resultado);
-    }).catch(console.error);
+    processaArquivo(caminho, valida);
   } else if (fs.lstatSync(caminho).isDirectory()) {
-    fs.promises.readdir(caminho).then(arquivos => {
-      arquivos.forEach(nomeDeArquivo => {
-        pegaArquivo(`${caminho}/${nomeDeArquivo}`).then(lista => {
-          imprimeLista(valida, lista, nomeDeArquivo);
-        }).catch(console.error);
-      })
-    }).catch(console.error);
+    processaDiretorio(caminho, valida);
   }
 }
 
