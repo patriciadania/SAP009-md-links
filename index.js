e36756053e25581da09860f2a8eaa3fe5d89d4d8
patriciadania <patricia.adania@hotmail.com>
2023-05-02 10:59:08 -0300

    Implementação de funções juntas

--- index.js
+++ index.js
@@ -1,36 +1,49 @@
 const fs = require('fs');
 const chalk = require('chalk');
+const fetchLink = require('node-fetch');
 
-function mdLinks(pathFile) {
-  return new Promise((resolve, reject) => {
-    const fileExists = fs.existsSync(pathFile);
-    const fileSize = fs.statSync(pathFile).size;
-
-    if (!fileExists || fileSize === 0) {
-      reject(chalk.red('\u2717') + ' ' + `O arquivo: ${chalk.red(pathFile)} está vazio ou não existe.`);
-    } else {
-      fs.readFile(pathFile, 'utf8', (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-          const searchLinks = data.match(regex);
-
-          const extraiLinks = searchLinks.map(link => {
-            const removeItens = link.replace(/.$/, '').replace('[', '');
-            const split = removeItens.split('](');
-            const newObj = {
-              href: split[1],
-              text: split[0],
-              file: pathFile,
-            };
-            return newObj;
-          });
-          resolve(extraiLinks);
-        }
-      });
-    }
-  });
-}
-
+function mdLinks(pathFile, options = {}) {
+    return new Promise((resolve, reject) => {
+      const fileExists = fs.existsSync(pathFile);
+      const fileSize = fs.statSync(pathFile).size;
+  
+      if (!fileExists || fileSize === 0) {
+        reject(chalk.red('\u2717') + ' ' + `O arquivo: ${chalk.red(pathFile)} está vazio ou não existe.`);
+      } else {
+        fs.readFile(pathFile, 'utf8', (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+            const searchLinks = data.match(regex);
+  
+            const extraiLinks = searchLinks.map(link => {
+              const removeItens = link.replace(/.$/, '').replace('[', '');
+              const split = removeItens.split('](');
+              const newObj = {
+                href: split[1],
+                text: split[0],
+                file: pathFile,
+              };
+              return newObj;
+            });
+  
+            if (options.validate) {
+              const promises = extraiLinks.map(element => fetchLink(element));
+              Promise.all(promises)
+                .then(linksArray => {
+                  resolve(linksArray);
+                })
+                .catch(error => {
+                  reject(error);
+                });
+            } else {
+              resolve(extraiLinks);
+            }
+          }
+        });
+      }
+    });
+  }
+  
 module.exports = mdLinks;
