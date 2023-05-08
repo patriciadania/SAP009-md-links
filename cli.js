61d6680e4a97ac9fe26d7639b1d51c08202f9e7b
patriciadania <patricia.adania@hotmail.com>
2023-05-08 18:41:40 -0300

    Refatoração de funções

--- cli.js
+++ cli.js
@@ -1,143 +1,57 @@
 #!/usr/bin/env node
 const chalk = require('chalk');
-const mdLinks = require('./index.js');
-const fetch = require('node-fetch');
+const { mdLinks, validateAndStats } = require('./index');
+const path = require('path');
 
 const pathFile = process.argv[2];
-const option = process.argv[3];
-
-const statusMessages = {
-  '200': 'OK',
-  '201': 'Criado',
-  '204': 'Nenhum conteúdo',
-  '400': 'Requisição inválida',
-  '401': 'Não autorizado',
-  '403': 'Proibido',
-  '404': 'Não encontrado',
-  '500': 'Erro interno do servidor',
-  '502': 'Gateway ruim',
-  '503': 'Serviço indisponível',
+const options = {
+  validate: process.argv.includes('--validate') || process.argv.includes('-v'),
+  stats: process.argv.includes('--stats') || process.argv.includes('-s'),
+  validateAndStats: process.argv.includes('--validate') && process.argv.includes('--stats'),
 };
 
-function fetchLink(element) {
-  if (!element || !element.href) {
-    return Promise.reject(new Error('Elemento inválido ou sem URL'));
-  }
-
-  return fetch(element.href)
-    .then(response => {
-      element.status = response.status;
-      element.statusText = statusMessages[response.status.toString()] || response.statusText;;
-      return element;
-    })
-    .catch(error => {
-      element.status = 'Não Encontrado';
-      element.statusText = error.message;
-      return element;
-    });
+function printLinks(links) {
+  links.forEach((link) => {
+    const statusColor =
+      link.status === 'FAIL' || link.status >= 400 ? chalk.red : chalk.green;
+    const status = link.status === undefined ? '' : ` ${link.status} ${statusColor(link.statusText || '')}`;
+    const output = `${chalk.blue(link.file)} ${chalk.cyan(link.href)}${status}`;
+    console.log(output);
+  });
 }
 
-
-function printStats(result) {
-  const linknique = [...new Set(result.map(element => element.href))];
-  const stats = {
-    total: result.length,
-    unique: linknique.length,
-  };
-  console.log(chalk.grey('Total:'), stats.total);
-  console.log(chalk.grey('Unique:'), stats.unique);
-};
-
-function printValidateResult (element) {
-  const statusColor = element.status >= 200 && element.status < 300 ? chalk.green : chalk.red;
-  console.log(
-    statusColor('\u2714'),
-    chalk.grey(element.file),
-    chalk.grey(element.href),
-    statusColor(`${element.status} ${element.statusText}`),
-    chalk.grey(element.text)
-  );
-};
-
-function printStatsWithBroken (result) {
-  const promises = result.map(element => fetchLink(element));
-
-  Promise.all(promises)
-    .then(linksArray => {
-      const linknique = [...new Set(linksArray.map(element => element.href))];
-      const stats = {
-        total: linksArray.length,
-        unique: linknique.length,
-        broken: linksArray.filter(element => element.status !== 200).length,
-      };
-      console.log(chalk.grey('Total:'), stats.total);
-      console.log(chalk.grey('Unique:'), stats.unique);
-      console.log(chalk.grey('Broken:'), stats.broken);
+if (options.validateAndStats) {
+  validateAndStats(pathFile, options)
+    .then((result) => {
+      console.log(chalk.blue(`Total: ${result.total}`));
+      console.log(chalk.cyan(`Unique: ${result.unique}`));
+      console.log(chalk.green(`OK: ${result.ok}`));
+      console.log(chalk.red(`Broken: ${result.broken}`));
     })
-    .catch(error => {
-      console.error(error);
+    .catch((error) => {
+      if (error.code === 'ENOENT') {
+        console.error(chalk.red(`File or directory "${path.relative(process.cwd(), pathFile)}" does not exist`));
+      } else {
+        console.error(chalk.red(error.message));
+      }
     });
-};
-
-function handleStatsWithValidateOption () {
-  mdLinks(pathFile)
-    .then(result => {
-      printStatsWithBroken(result);
-    })
-    .catch(error => {
-      console.log('Erro');
-      console.error(error);
-    });
-};
-
-function handleValidateOption () {
-  mdLinks(pathFile)
-    .then(result => {
-      const promises = result.map(element => fetchLink(element));
-
-      Promise.all(promises)
-        .then(linksArray => {
-          linksArray.forEach(element => {
-            printValidateResult(element);
-          });
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    })
-    .catch(error => {
-      console.log('Erro');
-      console.error(error);
-    });
-};
-
-function handleStatsOption() {
-  mdLinks(pathFile)
-    .then(result => {
-      printStats(result);
-    })
-    .catch(error => {
-      console.log('Erro');
-      console.error(error);
-    });
-};
-
-if (option === '--stats' && process.argv.includes('--validate')) {
-  handleStatsWithValidateOption();
-} else if (option === '--validate') {
-  handleValidateOption();
-} else if (option === '--stats') {
-  handleStatsOption();
 } else {
-// Se nenhuma opção for selecionada, imprime apenas os links
-mdLinks(pathFile)
-.then(result => {
-result.forEach(element => {
-console.log(chalk.grey(element.file), chalk.grey(element.href), chalk.grey(element.text));
-});
-})
-.catch(error => {
-console.log('Erro');
-console.error(error);
-});
+  mdLinks(pathFile, options)
+    .then((result) => {
+      if (result.length === 0) {
+        console.log(chalk.yellow('No links found in file or directory'));
+      } else if (options.stats) {
+        console.log(chalk.blue(`Total: ${result.total}`));
+        console.log(chalk.cyan(`Unique: ${result.unique}`));
+      } else {
+        printLinks(result.links);
+      }
+    })
+    .catch((error) => {
+      if (error.code === 'ENOENT') {
+        console.error(chalk.red(`File or directory "${path.relative(process.cwd(), pathFile)}" does not exist`));
+      } else {
+        console.error(chalk.red(error.message));
+      }
+    });
 }
--- index.js
+++ index.js
@@ -1,32 +1,105 @@
 const fs = require('fs');
+const path = require('path');
+const fetch = require('node-fetch');
 const chalk = require('chalk');
 
-function mdLinks(pathFile) {
+function mdLinks(pathFile, options = {}) {
   return new Promise((resolve, reject) => {
-    const fileExists = fs.existsSync(pathFile);
-    const fileSize = fs.statSync(pathFile).size;
+    const absolutePath = path.resolve(pathFile);
+    const fileExists = fs.existsSync(absolutePath);
+    const fileSize = fs.statSync(absolutePath).size;
 
     if (!fileExists || fileSize === 0) {
-      reject(chalk.red('\u2717') + ' ' + `O arquivo: ${chalk.red(pathFile)} está vazio ou não existe.`);
+      reject(new Error(chalk.red('\u2717') + ` O arquivo ${chalk.red(pathFile)} está vazio ou não existe.`));
     } else {
-      fs.readFile(pathFile, 'utf8', (err, data) => {
+      fs.readFile(absolutePath, 'utf8', (err, data) => {
         if (err) {
           reject(err);
         } else {
-          const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-          const searchLinks = data.match(regex);
+          const regex = /\[([^[\]]*)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+          const searchLinks = data.match(regex) || [];
 
-          const extraiLinks = searchLinks.map(link => {
-            const removeItens = link.replace(/.$/, '').replace('[', '');
-            const split = removeItens.split('](');
-            const newObj = {
-              href: split[1],
-              text: split[0],
-              file: pathFile,
+          const links = searchLinks.map((link) => {
+            const [href, text] = link.match(/\((https?:\/\/[^\s?#.].[^\s]*)\)/);
+            return {
+              href,
+              text,
+              file: absolutePath,
             };
-            return newObj;
           });
-          resolve(extraiLinks);
+
+          if (options.validate) {
+            const promises = links.map((link) => {
+              return fetch(link.href)
+                .then((response) => {
+                  link.status = response.status;
+                  link.statusText = response.statusText;
+                  return link;
+                })
+                .catch((error) => {
+                  link.status = error.code || 'FAIL';
+                  link.statusText = error.message;
+                  return link;
+                });
+            });
+
+            Promise.all(promises)
+              .then((result) => {
+                if (options.stats) {
+                  const uniqueLinks = [...new Set(result.map((link) => link.href))];
+                  const stats = {
+                    total: result.length,
+                    unique: uniqueLinks.length,
+                    broken: result.filter((link) => link.status !== 200).length,
+                  };
+                  resolve(stats);
+                } else {
+                  resolve(result);
+                }
+              })
+              .catch((error) => {
+                reject(error);
+              });
+          } else if (options.validateAndStats) {
+            const promises = links.map((link) => {
+              return fetch(link.href)
+                .then((response) => {
+                  link.status = response.status;
+                  link.statusText = response.statusText;
+                  return link;
+                })
+                .catch((error) => {
+                  link.status = error.code || 'FAIL';
+                  link.statusText = error.message;
+                  return link;
+                });
+            });
+
+            Promise.all(promises)
+              .then((result) => {
+                const uniqueLinks = [...new Set(result.map((link) => link.href))];
+                const stats = {
+                  total: result.length,
+                  unique: uniqueLinks.length,
+                  broken: result.filter((link) => link.status !== 200).length,
+                };
+                resolve(stats);
+              })
+              .catch((error) => {
+                reject(error);
+              });
+          } else {
+            if (options.stats) {
+              const uniqueLinks = [...new Set(links.map((link) => link.href))];
+              const stats = {
+                total: links.length,
+                unique: uniqueLinks.length,
+              };
+              resolve(stats);
+            } else {
+              resolve(links);
+            }
+          }
         }
       });
     }
